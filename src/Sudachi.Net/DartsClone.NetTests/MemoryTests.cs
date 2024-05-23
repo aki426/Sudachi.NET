c50bb6b59c0ca65b4f3627d61170721c8dc85e13
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-05-23 21:53:22 +0900

    test: Memory<>とSpan<>の使い方に関してテスト実施。

--- src/Sudachi.Net/DartsClone.NetTests/MemoryTests.cs
+++ src/Sudachi.Net/DartsClone.NetTests/MemoryTests.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Text;
-
+﻿using System.Runtime.InteropServices;
 using FluentAssertions;
 using Xunit;
-using DartsClone.Net;
-using System.Runtime.InteropServices;
 using Xunit.Abstractions;
-using System.IO.MemoryMappedFiles;
-using System.IO;
 
 namespace DartsClone.NetTests
 {
@@ -31,6 +20,34 @@ namespace DartsClone.NetTests
         private string ConvertToHexStr(byte[] byteArray)
             => string.Join(" - ", byteArray.Select(x => x.ToString("X2")));
 
+        private string ConvertToHexStr(Span<int> intSpan)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < intSpan.Length; i++)
+            {
+                result.Add(intSpan[i].ToString("X8"));
+            }
+
+            return string.Join(" - ", result);
+        }
+
+        private string ConvertToHexStr(Span<byte> byteSpan)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < byteSpan.Length; i++)
+            {
+                result.Add(byteSpan[i].ToString("X2"));
+            }
+
+            return string.Join(" - ", result);
+        }
+
+        // NOTE: GCを避けて高速なコードにするために、MemoryまたはSpanを使う必要がある。
+        // Spanをメンバーにしようとするとすべてref structになってしまうので、DoubleArrayの実装としては向かない。
+        // なお、SpanにはSlow SpanとFast Spanがあり、.NET Standard 2.0以前または.Net Frameworkの場合Slow Spanになってしまう。
+        // より高い性能が必要な場合は.NET Standard 2.1以降または.NET*へフレームワークを移したものを用意した方が良い。
+        // https://ufcpp.net/study/csharp/resource/span/
+
         // NOTE: intとbyteの型変換にはいくつかの方法がある。
         // 1. BitConverter.GetBytes(int) / BitConverter.ToInt32(byte[], int)
         // 2. Buffer.BlockCopy(byte[], int, byte[], int, int)
@@ -46,129 +63,85 @@ namespace DartsClone.NetTests
         // 一度読み込んだら内容を変更する際に多少コストがかかってもよい。
 
         [Fact]
-        public void SaveAndOpenMemoryTest()
+        public void MemoryTest()
         {
             var filePath = Path.Combine(Environment.CurrentDirectory, "SaveAndOpenMemoryTest.dat");
 
             #region メモリをファイルに保存する
 
+            // Memory / Spanの最初の起点は配列から作成する。
             int[] originalIntArray = new int[] { 1, 2, 4, 8, 65280, 2130706432 };
             Memory<int> intMemory = new Memory<int>(originalIntArray);
 
-            // Memory<int>をfilePathに保存
-            byte[] originalBytesArray = MemoryMarshal.AsBytes(intMemory.Span).ToArray();
-            Memory<byte> byteMemory = new Memory<byte>(originalBytesArray);
+            // Span<byte>は元のSpan<int>のビューとして振る舞う。
+            var byteSpan = MemoryMarshal.Cast<int, byte>(intMemory.Span);
 
-            // int[]とbyte[]の中身を確認
+            // int[], Memory<int>, Span<byte>の中身を確認
             _output.WriteLine($"original int[]: {originalIntArray.Length}");
             _output.WriteLine(ConvertToHexStr(originalIntArray));
             // intをHEX表示したときは普通に表示される（リトルエンディアン、ビッグエンディアンの区別はない）
             ConvertToHexStr(originalIntArray).Should().Be("00000001 - 00000002 - 00000004 - 00000008 - 0000FF00 - 7F000000");
 
-            _output.WriteLine($"original byte[]: {originalBytesArray.Length}");
-            _output.WriteLine(ConvertToHexStr(originalBytesArray));
+            _output.WriteLine($"original byte[]: {byteSpan.Length}");
+            _output.WriteLine(ConvertToHexStr(byteSpan));
             // バイト配列に変換するとリトルエンディアンになる。
-            ConvertToHexStr(originalBytesArray).Should().Be("01 - 00 - 00 - 00 - 02 - 00 - 00 - 00 - 04 - 00 - 00 - 00 - 08 - 00 - 00 - 00 - 00 - FF - 00 - 00 - 00 - 00 - 00 - 7F");
+            ConvertToHexStr(byteSpan).Should().Be("01 - 00 - 00 - 00 - 02 - 00 - 00 - 00 - 04 - 00 - 00 - 00 - 08 - 00 - 00 - 00 - 00 - FF - 00 - 00 - 00 - 00 - 00 - 7F");
 
-            // Memory<int>とMemory<byte>が実体を共有しているかチェック
-            intMemory.Span[0].Should().Be(1);
             intMemory.Span[0] = 256;
             intMemory.Span[0].Should().Be(256);
             // int[]とは共有する。
             originalIntArray[0].Should().Be(256);
+            // Span<byte>もビューに過ぎないのでメモリを共有している。
+            ConvertToHexStr(byteSpan.Slice(0, 4)).Should().Be("00 - 01 - 00 - 00");
 
-            // この方法ではMemory<int>とMemory<byte>はメモリを共有しない。
-            byteMemory.Span[0].Should().Be(1); // 0x01
-            byteMemory.Span[1].Should().Be(0); // 0x00
+            byteSpan[2] = 1;
+            _output.WriteLine(ConvertToHexStr(intMemory.Span));
+            intMemory.Span[0].Should().Be(65792);
+
+            // int[]とMemory<int>とSpan<byte>は実体を共有する。
+            ConvertToHexStr(originalIntArray).Should().Be(ConvertToHexStr(intMemory.Span));
+            ConvertToHexStr(byteSpan).Should().Be(ConvertToHexStr(MemoryMarshal.AsBytes(intMemory.Span)));
 
             // FileStreamを使ってファイルを開く
             using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
             {
-                // バイト配列をファイルに書き込む
-                // fs.Write(originalBytesArray, 0, originalBytesArray.Length);
-
-                // Memory<int>からbyte[]に変換して書き込む
-                var writeBytes = MemoryMarshal.AsBytes(intMemory.Span).ToArray();
-                fs.Write(writeBytes, 0, writeBytes.Length);
+                // FileStreamにはReadOnlySpan<byte>を直接書き込むWrite関数がある。便利。
+                fs.Write(MemoryMarshal.Cast<int, byte>(intMemory.Span));
             }
 
             #endregion メモリをファイルに保存する
 
             #region ファイルをメモリに読み込む - 配列に変換する方法。
 
+            // NOTE: IOがbyte単位なので、Memory<int>はbyte[] -> Memory<byte> -> Span<byte> -> Memory<int>と変換するのが自然。
+            // これはJAVA版のByteBuffer.asIntBufferに通じるものがある。
+
+            // NOTE: JAVA版ではFileChannel.mapを用いてByteBufferをFileと同期させているが、この機能が必要とも思えない。
+            // C#版ではMemoryMappedFileを使うことで同様の機能を実現できるが、その必要性があるかは検証不足。
+
             // ファイルからバイト配列を読み込む
             byte[] bytesLoaded = File.ReadAllBytes(filePath);
-
+            MemoryMarshal.Cast<byte, int>(bytesLoaded);
             // バイト配列をMemory<byte>に変換
             Memory<byte> byteMemoryLoaded = new Memory<byte>(bytesLoaded);
 
-            // バイト配列をMemory<int>に変換
-            // NOTE: MemoryMarshal.CastはSpanを経由してしまうがこれが良いのかどうか検証不足。
-            // Span<int> intMemoryLoaded = MemoryMarshal.Cast<byte, int>(bytes);
-            byte[] temp = new byte[sizeof(int)];
-            Memory<int> intMemoryLoaded = new Memory<int>(Enumerable.Range(0, bytesLoaded.Length / sizeof(int))
-                .Select(i =>
-                {
-                    Buffer.BlockCopy(bytesLoaded, i * sizeof(int), temp, 0, sizeof(int));
-                    return BitConverter.ToInt32(temp, 0);
-                })
-                .ToArray());
+            // おそらくMemory<byte>を生成した場合は、Memory<int>を別途持つ必要は無いはず。
+            Span<int> intSpanLoaded = MemoryMarshal.Cast<byte, int>(byteMemoryLoaded.Span);
 
-            // assertion
-            byteMemoryLoaded.Length.Should().Be(24);
-            intMemoryLoaded.Length.Should().Be(6);
+            byteMemoryLoaded.Span[0].Should().Be(0);
+            byteMemoryLoaded.Span[1].Should().Be(1);
+            byteMemoryLoaded.Span[2].Should().Be(1);
 
-            _output.WriteLine($"loaded int[]: {intMemoryLoaded.Length}");
-            _output.WriteLine(ConvertToHexStr(intMemoryLoaded.Span.ToArray()));
-            // _output.WriteLine(ConvertToHexStr(intMemoryLoaded.ToArray()));
-            ConvertToHexStr(intMemoryLoaded.Span.ToArray()).Should().Be(ConvertToHexStr(originalIntArray));
+            intSpanLoaded[0].Should().Be(65792);
+            intSpanLoaded[1].Should().Be(2);
 
-            _output.WriteLine($"loaded byte[]: {byteMemoryLoaded.Length}");
-            _output.WriteLine(ConvertToHexStr(byteMemoryLoaded.Span.ToArray()));
-            // _output.WriteLine(ConvertToHexStr(byteMemoryLoaded.ToArray()));
-            // ファイルはoriginalIntArrayもしくはintMemoryと内容が一致するのでそちらでマッチングを見る。
-            ConvertToHexStr(byteMemoryLoaded.Span.ToArray())
-                .Should().Be(ConvertToHexStr(MemoryMarshal.AsBytes(intMemory.Span).ToArray()));
+            byteMemoryLoaded.Span[4] = 8;
+            intSpanLoaded[1].Should().Be(8);
 
-            // 読み込んだ値のチェック
-            intMemoryLoaded.Span[0].Should().Be(256);
-            byteMemoryLoaded.Span[0].Should().Be(0); // 0x00
-            byteMemoryLoaded.Span[1].Should().Be(1); // 0x01 => 0x0100 = 256
-
-            // 変更
-            intMemoryLoaded.Span[0] = 1;
-            intMemoryLoaded.Span[0].Should().Be(1);
-
-            // この方法ではMemory<int>とMemory<byte>はメモリを共有しない。
-            byteMemoryLoaded.Span[0].Should().Be(0); // 0x00
-            byteMemoryLoaded.Span[1].Should().Be(1); // 0x01
+            intSpanLoaded[1] = 15;
+            byteMemoryLoaded.Span[4].Should().Be(15);
 
             #endregion ファイルをメモリに読み込む - 配列に変換する方法。
-
-            #region ファイルをメモリに読み込む - Memory関係のクラスを使う方法。
-
-            //using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(fileInfo.FullName, FileMode.Open, null, totalSize, MemoryMappedFileAccess.Read))
-            //{
-            //    using (MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor(position, totalSize, MemoryMappedFileAccess.Read))
-            //    {
-            //        var buffer = new byte[totalSize];
-            //        accessor.ReadArray<byte>(0, buffer, 0, buffer.Length);
-            //        this.buffer = new Memory<byte>(buffer);
-
-            //        if (this.buffer.Length % sizeof(int) != 0)
-            //        {
-            //            throw new ArgumentException("Buffer length must be a multiple of sizeof(int).");
-            //        }
-
-            //        var intBuffer = new int[this.buffer.Length / sizeof(int)];
-            //        accessor.ReadArray<int>(0, intBuffer, 0, intBuffer.Length);
-            //        var intArray = new Memory<int>(intBuffer);
-
-            //        int size = intArray.Length;
-            //    }
-            //}
-
-            #endregion ファイルをメモリに読み込む - Memory関係のクラスを使う方法。
         }
     }
 }
